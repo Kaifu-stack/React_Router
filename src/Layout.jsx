f208400e3db62b721f1a3b5ec2dca6726cd400ba
Kaifu-stack <kaif22101@gmail.com>
2025-08-27 02:50:21 +0530

    done

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header/Header'
-import Footer from './components/Footer/footer'
+import Footer from './components/Footer/Footer_temp.jsx'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
